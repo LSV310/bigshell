47727ca4ee5c0dadf4151a3026ed1c14e5a11ba5
Anicet Gruet <agruet@paul-f4Br5s6.clusters.42paris.fr>
2025-02-24 15:28:22 +0100

    Nothing important

--- srcs/pipex/pipex.c
+++ srcs/pipex/pipex.c
@@ -6,35 +6,29 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 17:28:41 by agruet            #+#    #+#             */
-/*   Updated: 2025/02/21 18:15:50 by agruet           ###   ########.fr       */
+/*   Updated: 2025/02/24 13:07:59 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	wait_child(pid_t pid, int *exit_code, int last)
+int	wait_childs(int cmd_amount)
 {
 	int	status;
+	int	i;
 
-	if (pid == 0)
-	{
-		*exit_code = 127;
-		ft_fprintf(2, "pipex: command not found\n");
-		return ;
-	}
-	if (!last)
-		waitpid(pid, &status, WNOHANG);
-	else
-		waitpid(pid, &status, 0);
+	i = 0;
+	while (i++ < cmd_amount)
+		waitpid(-1, &status, 0);
 	if (WIFEXITED(status))
-		*exit_code = WEXITSTATUS(status);
+		return (WEXITSTATUS(status));
 	else if (WIFSIGNALED(status))
-		*exit_code = 128 + WTERMSIG(status);
+		return (128 + WTERMSIG(status));
 	else
-		*exit_code = 1;
+		return (1);
 }
 
-pid_t	exec_cmd(char *cmd, int *pipefd, int outfile)
+pid_t	exec_cmd(char *cmd, int *pipefd)
 {
 	char	*cmd;
 	char	**args;
@@ -51,7 +45,7 @@ pid_t	exec_cmd(char *cmd, int *pipefd, int outfile)
 	{
 		// Free everything
 		dup2(pipefd[1], STDOUT_FILENO);
-		(close(pipefd[0]), close(pipefd[1]), close(outfile));
+		(close(pipefd[0]), close(pipefd[1]));
 		execve(cmd, args, __environ);
 		perror("pipex");
 		exit(EXIT_FAILURE);
@@ -60,26 +54,23 @@ pid_t	exec_cmd(char *cmd, int *pipefd, int outfile)
 	return (pid);
 }
 
-int	pipex(char **cmds, int nbr, int outfile)
+int	pipex(char **cmds, int infile, int outfile)
 {
 	int	pipefd[2];
 	int	exit_code;
 	int	i;
 
-	exit_code = 0;
-	pipe(pipefd);
-	exec_cmd(cmds[0], pipefd, outfile);
-	i = 1;
-	while (i < nbr)
+	i = 0;
+	while (cmds[i])
 	{
-		dup2(pipefd[0], STDIN_FILENO);
-		(close(pipefd[0]), close(pipefd[1]));
+		// open REDIN / REDOUT of the current function
+		dup2(pipefd[0], STDIN_FILENO); //conditional: either pipefd[0] or redin or nothing (if first)
+		(close(pipefd[0]), close(pipefd[1])); // only if i > 0
 		pipe(pipefd);
-		if (i == nbr - 1)
-			dup2(outfile, pipefd[1]);
-		exec_cmd(cmds[i], pipefd, outfile);
-		i++;
+		// if redout dup2(redout, pipefd[1])
+		exec_cmd(cmds[i], pipefd);
 	}
 	(close(pipefd[0]), close(pipefd[1]));
-	return (exit_code);
+	(close(infile), close(outfile));
+	return (wait_childs(i));
 }
