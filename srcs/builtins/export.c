5ed8b0baac413266a0219b231363ac8c61a69ce4
LSV310 <anicet.gruet@gmail.com>
2025-03-11 17:23:46 +0100

    update export

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,37 +6,48 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 11:17:55 by agruet            #+#    #+#             */
-/*   Updated: 2025/03/07 11:49:02 by agruet           ###   ########.fr       */
+/*   Updated: 2025/03/11 17:12:30 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	export(t_mini *minishell, char *var)
+static int	create_new_var(t_mini *minishell, char *var)
 {
 	t_map	*new;
+
+	new = newmap(NULL, NULL);
+	if (!new)
+		return (0);
+	if (!assign_kv(var, new))
+		return (free(new), 0);
+	ft_addmap(&minishell->env, new);
+	return (1);
+}
+
+static int	replace_var(t_map *find, char *var, char *chr)
+{
+	free(find->value);
+	find->value = ft_substr(var, chr - var + 1, ft_strlen(var));
+	if (!find->value)
+		return (0);
+	return (1);
+}
+
+int	export(t_mini *minishell, char *var)
+{
 	t_map	*find;
 	char	*chr;
 
+	if (!var)
+		return (env(minishell));
 	chr = ft_strchr(var, '=');
 	if (!chr)
 		return (1);
 	find = get_env(minishell, var, chr - var);
-	if (!find)
-	{
-		new = newmap(NULL, NULL);
-		if (!new)
-			return (1);
-		if (!assign_kv(var, new))
-			return (free(new), 1);
-		ft_addmap(&minishell->env, new);
-	}
-	else
-	{
-		free(find->value);
-		find->value = ft_substr(var, chr - var + 1, ft_strlen(var));
-		if (!find->value)
-			return (1);
-	}
+	if (!find && !create_new_var(minishell, var))
+		return (1);
+	else if (!replace_var(find, var, chr))
+		return (1);
 	return (0);
 }
--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 11:31:31 by agruet            #+#    #+#             */
-/*   Updated: 2025/03/07 11:49:02 by agruet           ###   ########.fr       */
+/*   Updated: 2025/03/11 17:18:02 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	unset(t_mini *minishell, char *var)
 
 	find = get_env(minishell, var, ft_strlen(var));
 	if (!find)
-		return (1);
+		return (0);
 	map_remove_node(&minishell->env, find);
 	return (0);
 }
