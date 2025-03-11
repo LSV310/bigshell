eb433956a23cfdd56ba13e8868374c38780dd905
LSV310 <anicet.gruet@gmail.com>
2025-03-11 15:57:32 +0100

    echo -nnnnnn fix

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,24 +6,49 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 15:25:51 by agruet            #+#    #+#             */
-/*   Updated: 2025/03/11 14:40:43 by agruet           ###   ########.fr       */
+/*   Updated: 2025/03/11 15:56:55 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	echo(char **args, int param)
+static int	find_n_arg(char *arg)
 {
 	int	i;
 
 	i = 1;
+	while (arg[i] && arg[i] == 'n')
+		i++;
+	if (arg[i])
+		return (false);
+	return (true);
+}
+
+int	echo(char **args)
+{
+	int		current_arg;
+	bool	n_arg;
+
 	if (!args)
 		return (0);
-	while (args[i] && args[i + 1])
-		ft_printf("%s ", args[i++]);
-	if (args[i])
-		ft_printf("%s", args[i]);
-	if (!param)
+	n_arg = false;
+	current_arg = 0;
+	while (args[current_arg])
+	{
+		if (args[current_arg][0] != '-')
+			break ;
+		n_arg = find_n_arg(args[current_arg]);
+		if (n_arg == false)
+			break ;
+		current_arg++;
+	}
+	if (current_arg != 0)
+		n_arg = true;
+	while (args[current_arg] && args[current_arg + 1])
+		ft_printf("%s ", args[current_arg++]);
+	if (args[current_arg])
+		ft_printf("%s", args[current_arg]);
+	if (!n_arg)
 		ft_printf("\n");
 	return (0);
 }
