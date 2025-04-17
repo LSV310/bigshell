3be3ca42bfdc41aa01616730ec3220d66f6e82ff
LSV310 <anicet.gruet@gmail.com>
2025-04-17 14:40:50 +0200

    WTF ? Ok so last commit but fuck git

--- srcs/utils/wildcards.c
+++ srcs/utils/wildcards.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wildcards.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tgallet <tgallet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 11:23:42 by agruet            #+#    #+#             */
-/*   Updated: 2025/03/28 13:44:39 by agruet           ###   ########.fr       */
+/*   Updated: 2025/04/17 14:34:05 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,33 +39,54 @@ DIR	*search_directory(t_list **lst, t_wildcard_type type)
 	return (dir);
 }
 
-bool	wildcard_cmp(char *content, char *pattern)
+void	finish_cmp(t_list **lst, t_list *elem, char *content, char *chr)
 {
-	if (*pattern == '\0')
+	char	*str;
+	char	*find;
+
+	while (*chr == '*')
+		chr++;
+	str = chr;
+	chr = ft_strchr(str, '*');
+	while (chr)
 	{
-		if (*content == '\0')
-			return (true);
-		return (false);
+		find = ft_strlstr(content, str, chr - str);
+		if (!find)
+		{
+			lst_remove_node(lst, elem, &void_content);
+			return ;
+		}
+		content = find + (chr - str);
+		str = chr;
+		while (str && *str == '*')
+			str++;
+		chr = ft_strchr(str, '*');
 	}
-	else if (*pattern == '*')
-		return ((*content && wildcard_cmp(content + 1, pattern))
-			|| wildcard_cmp(content, pattern + 1));
-	else
-	{
-		if (*pattern == *content)
-			return (wildcard_cmp(content + 1, pattern + 1));
-		else
-			return (false);
-	}
+	if (*str && ft_strrncmp(content, str, ft_strlen(str)))
+		lst_remove_node(lst, elem, &void_content);
 }
 
 void	compare_str(t_list **lst, t_list *elem, char *str)
 {
 	char	*content;
+	char	*chr;
 
 	content = elem->content;
-	if (!wildcard_cmp(content, str))
+	chr = ft_strchr(str, '*');
+	if (!chr)
+	{
+		if (ft_strncmp(str, content, ft_max(ft_strlen(str),
+					ft_strlen(content))))
+			lst_remove_node(lst, elem, &void_content);
+		return ;
+	}
+	else if (ft_strncmp(str, content, chr - str))
+	{
 		lst_remove_node(lst, elem, &void_content);
+		return ;
+	}
+	content += chr - str;
+	finish_cmp(lst, elem, content, chr);
 }
 
 char	*get_expanded(t_list *lst, DIR *dir, char *str)
@@ -84,6 +105,8 @@ char	*get_expanded(t_list *lst, DIR *dir, char *str)
 		current = current->next;
 	}
 	result = ft_calloc(count + 1, sizeof(char));
+	if (!result)
+		return (free_wildcards(lst, dir), NULL);
 	current = lst;
 	while (current)
 	{
@@ -92,7 +115,6 @@ char	*get_expanded(t_list *lst, DIR *dir, char *str)
 		ft_strlcat(result, " ", ft_strlen(result) + 2);
 		current = current->next;
 	}
-	result[count - 1] = 0;
 	return (free_wildcards(lst, dir), result);
 }
 
